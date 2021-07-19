10a30aa82798189489e4266cbd0c49192478e552
kubilayturgut <kubilay.turgut@gmail.com>
2021-07-19 12:07:04 +0900

    1st commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vue3PanoramaSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  Vue3PanoramaSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-panorama-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Vue3PanoramaSample } from './vue3-panorama-sample.vue';
